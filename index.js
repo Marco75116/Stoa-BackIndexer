f34111840cd779091c424345b758184da34fd95c
marc-antoine ourradour <ourradourm@gmail.com>
2023-05-29 04:02:31 +0200

    stange bugs related to time in railway - test

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { listenDiamond } = require("./utils/helpers/ethers.helper/event.helper");
+const { timeSerializer } = require("./utils/helpers/index.helper/index.helper");
 require("dotenv").config();
 
 async function main() {
-  console.log("In");
+  const currentDate = new Date().getTime();
+  const timestamp = timeSerializer(currentDate);
+  console.log(timestamp);
   listenDiamond();
 }
 
